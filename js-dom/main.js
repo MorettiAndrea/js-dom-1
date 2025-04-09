6cdf73b16bb705ca96cc697444af1f176cb5ec46
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-09 18:00:42 +0200

    fine bonus

--- js-dom/main.js
+++ js-dom/main.js
@@ -4,11 +4,25 @@ const lampOn = document.getElementById("lamp-on");
 lampOn.style.display = "none";
 lampOff.style.display = "block";
 
+// #### esercizio principale da decommentare ####
+
+// lampSwitchButton.addEventListener("click", function () {
+//   if (lampOff.style.display === "none") {
+//     lampOff.style.display = "block";
+//   } else {
+//     lampOff.style.display = "none";
+//   }
+
+//   return lampSwitchButton;
+// });
+
+// #### bonus ####
+
 lampSwitchButton.addEventListener("click", function () {
   if (lampOff.style.display === "none") {
-    lampOff.style.display = "block";
+    (lampOff.style.display = "block") && (lampOn.style.display = "none");
   } else {
-    lampOff.style.display = "none";
+    (lampOff.style.display = "none") && (lampOn.style.display = "block");
   }
 
   return lampSwitchButton;
